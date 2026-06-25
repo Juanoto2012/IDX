49b5a68d2f5056e62aa436b7676dbfa43c13dfe4
Juanoto2012 <jntxstudio@gmail.com>
2026-06-25 12:35:44 -0600

    Add window state persistence and workspace restore functionality

--- preload.js
+++ preload.js
@@ -4,13 +4,11 @@ contextBridge.exposeInMainWorld('electronAPI', {
   minimize: () => ipcRenderer.send('window-minimize'),
   maximize: () => ipcRenderer.send('window-maximize'),
   close: () => ipcRenderer.send('window-close'),
-  
   terminalInput: (data) => ipcRenderer.send('terminal-input', data),
   onTerminalOutput: (callback) => ipcRenderer.on('terminal-output', (event, data) => callback(data)),
   resizeTerminal: (cols, rows) => ipcRenderer.send('terminal-resize', {cols, rows}),
-  
   getOsInfo: () => ipcRenderer.invoke('get-os-info'),
-  
   checkForUpdates: () => ipcRenderer.invoke('check-updates'),
-  getAppVersion: () => ipcRenderer.invoke('get-app-version')
+  getAppVersion: () => ipcRenderer.invoke('get-app-version'),
+  getDirectoryHandle: (path) => ipcRenderer.invoke('get-directory-handle', path)
 });
